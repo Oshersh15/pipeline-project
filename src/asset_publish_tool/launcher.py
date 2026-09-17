c489d69f54a9c7b3e032595ab1748b8edeadf445
oshersh15 <osher.shechter@gmail.com>
2026-09-17 14:36:59 +0100

    Fix Maya module reload ordering

--- src/asset_publish_tool/launcher.py
+++ src/asset_publish_tool/launcher.py
@@ -1,19 +1,25 @@
 import importlib
 
-import asset_publish_tool.core.asset as asset
-import asset_publish_tool.database.asset_repository as asset_repository
-import asset_publish_tool.database.connection as connection
-import asset_publish_tool.maya.publisher as publisher
-import asset_publish_tool.ui.maya_pyside_ui as ui
-import asset_publish_tool.usd.usd_utils as usd_utils
-
 
 def show():
+    import asset_publish_tool.core.asset as asset
+    import asset_publish_tool.core.naming as naming
+    import asset_publish_tool.database.asset_repository as asset_repository
+    import asset_publish_tool.database.connection as connection
+    import asset_publish_tool.usd.usd_utils as usd_utils
+
     importlib.reload(asset)
+    importlib.reload(naming)
     importlib.reload(connection)
     importlib.reload(asset_repository)
     importlib.reload(usd_utils)
+
+    import asset_publish_tool.maya.publisher as publisher
+
     importlib.reload(publisher)
+
+    import asset_publish_tool.ui.maya_pyside_ui as ui
+
     importlib.reload(ui)
 
     ui.show_ui()
